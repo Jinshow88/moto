388e2a50c3b8d07ffe4c08a79dbfa0555771eb02
이진현 <ljun88@naver.com>
2024-10-16 14:31:17 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className = "mein">
+      <h1>당근</h1>
+     </div>
+    </div>
+  );
+}
+
+export default App;
